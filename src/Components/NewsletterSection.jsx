de1880627ec9a7d8fad55d6549d18920f1a4c0db
shoowack <isuvak@gmail.com>
2020-10-24 23:09:26 -0400

    styling newsletter component

--- src/Components/NewsletterSection.jsx
+++ src/Components/NewsletterSection.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import "./newsletter.scss";
 
 function NewsletterSection() {
   return (
-    <div className="container py-3 py-md-5 text-center">
+    <div className="newsletter container py-3 py-md-5 text-center">
       <div className="row">
         <div className="col-12">
           <h3>Stay informed with our newsletter</h3>
-          <p>Subscribe to get news about new releases and special offers</p>
-          <form action="" method="post" encType="multipart/form-data">
-            <input type="email" name="email" placeholder="Your E-mail" />
-            <input type="submit" name="submit" value="Subscribe" />
-          </form>
+          <small>
+            Subscribe to get news about new releases and special offers
+          </small>
+          <div className="shadow d-inline-block mt-4">
+            <form action="" method="post" encType="multipart/form-data">
+              <input type="email" name="email" placeholder="Your E-mail" />
+              <input type="submit" name="submit" value="Subscribe" />
+            </form>
+          </div>
         </div>
       </div>
     </div>
