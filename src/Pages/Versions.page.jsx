e18df0e9496f9ac35df446f1984f9607d8d3bfc4
shoowack <isuvak@gmail.com>
2020-10-25 20:03:22 -0400

    addition to prev commit

--- src/Pages/Versions.page.jsx
+++ src/Pages/Versions.page.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
 import TitleHeader from "./../Components/TitleHeader";
-import versions from "./../../src/CHANGELOG.md";
+// import versions from "./../../src/Data/CHANGELOG.md";
+import versions from "/CHANGELOG.md";
 
 function VersionsPage() {
+  const [changelog, setChangelog] = useState("Loading...");
+
+  useEffect(() => {
+    fetch(versions)
+      .then((response) => response.text())
+      .then((text) => setChangelog(text));
+  }, []);
+
   return (
     <>
       <TitleHeader>Version History</TitleHeader>
       <div className="container py-3 py-md-5">
         <div className="row">
           <div className="col-12">
-            <ReactMarkdown plugins={[gfm]}>{versions}</ReactMarkdown>
+            <ReactMarkdown plugins={[gfm]} source={changelog} />
           </div>
         </div>
       </div>
