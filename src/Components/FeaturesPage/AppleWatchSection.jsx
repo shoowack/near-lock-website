64fa7b6f1d1be5926b684fdb84858a0e99f9dce9
shoowack <isuvak@gmail.com>
2020-10-27 00:24:06 -0400

    fixing apple watch section on features page

--- src/Components/FeaturesPage/AppleWatchSection.jsx
+++ src/Components/FeaturesPage/AppleWatchSection.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import FeatureContainer from "./../FeatureContainer";
+import Slider from "react-slick";
+import "./../Sliders/slick.scss";
+import "./../Sliders/slick-theme.scss";
 
 function AppleWatchSection() {
   return (
@@ -10,37 +13,45 @@ function AppleWatchSection() {
       }
     >
       <div className="row">
-        <a
-          href="https://itunes.apple.com/us/app/near-lock/id886882252?mt=8"
-          className="btn"
-        >
-          Download now
-        </a>
+        <div className="col-12">
+          <a
+            href="https://itunes.apple.com/us/app/near-lock/id886882252?mt=8"
+            className="btn"
+          >
+            Download now
+          </a>
+        </div>
         <div className="apple-watch">
           <img
-            src="img/features-watch.png"
+            src="/feature/features-watch.png"
             className="apple-watch-image"
             alt=""
           />
           <div className="screen-wrapper">
             <div className="screen">
-              <div id="owl-apple-watch" className="owl">
+              <Slider
+                dots={false}
+                infinite={true}
+                speed={500}
+                slidesToShow={1}
+                slidesToScroll={1}
+              >
                 <div className="item">
-                  <img src="img/watch-1.jpg" alt="" />
+                  <img src="/feature/watch-1.jpg" alt="" />
                 </div>
                 <div className="item">
-                  <img src="img/watch-2.jpg" alt="" />
+                  <img src="/feature/watch-2.jpg" alt="" />
                 </div>
                 <div className="item">
-                  <img src="img/watch-3.jpg" alt="" />
+                  <img src="/feature/watch-3.jpg" alt="" />
                 </div>
                 <div className="item">
-                  <img src="img/watch-4.jpg" alt="" />
+                  <img src="/feature/watch-4.jpg" alt="" />
                 </div>
                 <div className="item">
-                  <img src="img/watch-5.jpg" alt="" />
+                  <img src="/feature/watch-5.jpg" alt="" />
                 </div>
-              </div>
+              </Slider>
             </div>
           </div>
         </div>
