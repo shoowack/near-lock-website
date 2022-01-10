f4614eb748927cc4fa8f8904f1228e3447e903ee
Ivan Šuvak Martinović <isuvak@gmail.com>
2022-01-10 00:07:53 -0500

    added eslint

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -1,11 +1,11 @@
-import TopSection from "./TopSection";
-import FamilySection from "./FamilySection";
-import DownloadsSection from "./DownloadsSection";
-import ToolsSection from "./ToolsSection";
-import FeaturesSection from "./FeaturesSection";
-import CompatibilitySection from "./CompatibilitySection";
-import TestimonialsSection from "./TestimonialsSection";
-import ProblemsSection from "./ProblemsSection";
+import TopSection from './TopSection';
+import FamilySection from './FamilySection';
+import DownloadsSection from './DownloadsSection';
+import ToolsSection from './ToolsSection';
+import FeaturesSection from './FeaturesSection';
+import CompatibilitySection from './CompatibilitySection';
+import TestimonialsSection from './TestimonialsSection';
+import ProblemsSection from './ProblemsSection';
 
 export {
   TopSection,
@@ -15,5 +15,5 @@ export {
   ToolsSection,
   FeaturesSection,
   CompatibilitySection,
-  TestimonialsSection
+  TestimonialsSection,
 };
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,16 +1,4 @@
 import { lazy } from "react";
-
-// import UnderConstructionPage from "./UnderConstructionPage/UnderConstruction.page";
-// import HomePage from "./Home.page";
-// import FaqPage from "./Faq.page";
-// import FeaturesPage from "./Features.page";
-// import VersionsPage from "./Versions.page";
-// import PressPage from "./Press.page";
-// import TermsOfUsePage from "./TermsOfUse.page";
-// import ImpressumPage from "./Impressum.page";
-// import ErrorPage from "./Error.page";
-// import ProblemsPage from "./Problems.page";
-
 const UnderConstructionPage = lazy(() =>
   import("./UnderConstructionPage/UnderConstruction.page")
 );
