a09f2616d28dc5bdcfc07d7d201f7958d2bbcd46
Ivan Šuvak Martinović <isuvak@gmail.com>
2022-02-10 21:56:44 -0500

    trying to get the site running on netlify

--- next.config.js
+++ next.config.js
@@ -1,40 +1,4 @@
-const path = require('path');
 const nextTranslate = require('next-translate');
-const withMdxEnhanced = require('next-mdx-enhanced');
-
-module.exports = withMdxEnhanced({
-  layoutPath: 'src/layouts',
-  defaultLayout: true,
-  // rehypePlugins: [rehypePrism],
-})({
-  pageExtensions: ['mdx', 'tsx'],
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.module.rules.push(
-      ...[
-        {
-          test: /\.yml$/,
-          type: 'json',
-          use: 'yaml-loader',
-        },
-        {
-          test: /\.svg$/,
-          use: '@svgr/webpack',
-        },
-      ],
-    );
-    return config;
-  },
-});
-
-module.exports = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  module: {
-    rules: [{ test: /\.md$/, use: 'raw-loader' }],
-  },
-};
 
 module.exports = nextTranslate({
   async rewrites() {
